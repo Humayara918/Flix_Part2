fde14760fa113c6ea7830f8699bf2463717b7870
Humayara918 <64216341+Humayara918@users.noreply.github.com>
2021-02-25 19:19:32 -0500

    initial commit

---
+++ MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Humayara Karim on 2/25/21.
+//  Copyright © 2021 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+  
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
